26e00876594caa01fb1633281b763414937df9e0
gorillabits <admin@gorillabits.net>
2022-05-04 16:38:32 +0100

    price test update

--- backend/src/config.js
+++ backend/src/config.js
@@ -55,7 +55,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x962385B7B8996C7D517224F9e133964392D70114';
 const TREASURY_ADDRESS = '0x962385B7B8996C7D517224F9e133964392D70114';
 const MAX_SUPPLY = 1000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.3; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
